313720f06de53e2f54f1e91205db6bba91be0eb9
Yuri Batan <batanyuri@gmail.com>
2024-06-05 14:41:24 -0500

    Printed out func_before and func_after for the first row

--- BigVul/bigvul_parser.py
+++ BigVul/bigvul_parser.py
@@ -2,7 +2,14 @@ import csv
 
 import pandas as pd
 
+
 file = "Dataset/MSR_data_cleaned.csv"
 
 df = pd.read_csv(file, nrows = 1)
-print(df.to_string())
+
+if 'func_before' in df.columns and 'func_after' in df.columns:
+    selected_columns = df[['func_before', 'func_after']]
+    first_row = selected_columns.iloc[0]
+
+    print(f"func_before: {first_row['func_before']}")
+    print(f"func_after: {first_row['func_after']}")
