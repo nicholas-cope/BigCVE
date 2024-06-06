178127797ab1aca093a79b416550f6809518dd49
nicholas-cope <nicholascope2709@gmail.com>
2024-06-06 14:24:59 -0500

    adding

--- CVEFixes/cve_parser.py
+++ CVEFixes/cve_parser.py
@@ -11,7 +11,7 @@ from pathlib import Path
 # import difflib as diff
 # import re
 # import csv
-# import ast@
+# import ast
 
 def create_connection(db_file):
     """
@@ -26,16 +26,37 @@ def create_connection(db_file):
         print(e)
     return conn
 
+#
+# DATA_PATH = Path.cwd() / 'Data'
+#
+#
+# Path(DATA_PATH).mkdir(parents=True, exist_ok=True)
 
-DATA_PATH = Path.cwd().parents[0] / 'Data'
+
+# conn = create_connection(DATA_PATH / "CVEfixes.db")
+
+conn = create_connection(Path.cwd() / "CVEfixes_v1.0.7" / "Data" / "CVEfixes.db")
+
+# df_c_methods = pd.read_sql_query("SELECT m.name, m.signature, m.nloc, \
+# m.parameters, m.token_count, m.code, m.before_change, f.programming_language FROM method_change m, file_change f \
+# WHERE f.file_change_id=m.file_change_id AND f.programming_language='C'", conn)
+
+all_methods = pd.read_sql_query("SELECT m.code, m.before_change, f.programming_language \
+FROM method_change m, file_change f \
+WHERE f.file_change_id=m.file_change_id AND (f.programming_language='C' OR f.programming_language='C++')", conn)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_colwidth', None)
+
+vul_methods = pd.read_sql_query("SELECT m.code \
+FROM method_change m, file_change f \
+WHERE f.file_change_id=m.file_change_id AND m.before_change='True' AND (f.programming_language='C' OR f.programming_language='C++')", conn)
+
+fixed_methods = pd.read_sql_query("SELECT m.code \
+FROM method_change m, file_change f \
+WHERE f.file_change_id=m.file_change_id AND m.before_change='False' AND (f.programming_language='C' OR f.programming_language='C++')", conn)
 
 
-Path(DATA_PATH).mkdir(parents=True, exist_ok=True)
-
-
-conn = create_connection(DATA_PATH / "CVEfixes.db")
-
-df_c_methods = pd.read_sql_query("SELECT m.name, m.signature, m.nloc, \
-m.parameters, m.token_count, m.code, m.before_change, f.programming_language FROM method_change m, file_change f \
-WHERE f.file_change_id=m.file_change_id AND f.programming_language='C'", conn)
-df_c_methods.head(5)
+print(vul_methods)
+print(fixed_methods)
