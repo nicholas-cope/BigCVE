de076532fea30ea12e6a818d1c99c0742fd7e033
Yuri Batan <batanyuri@gmail.com>
2024-06-06 09:03:56 -0500

    Using a chunks based approach to optimize performance of parser

--- BigVul/bigvul_parser.py
+++ BigVul/bigvul_parser.py
@@ -2,16 +2,10 @@ import csv
 
 import pandas as pd
 
-
+#Path to BigVul Dataset
 file = "Dataset/MSR_data_cleaned.csv"
+#How many rows we are going to read at a time
+#Optimizing performance
+chunks = 25000
 
-df = pd.read_csv(file, nrows=10000)
 
-if 'func_before' in df.columns and 'func_after' in df.columns:
-    if 'func_before' in df.columns and 'func_after' in df.columns:
-        for index, row in df.iterrows():
-            print(f"\n--- Row {index + 1} ---")
-            print(f"func_before: {row['func_before']}")
-            print(f"func_after: {row['func_after']}")
-    else:
-        print("Error: One or both columns 'func_before' and 'func_after' were not found in the CSV file.")
