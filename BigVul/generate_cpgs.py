ded9a5f48bf5df8c55aa2ba36c1e5dd45ae64f63
Yuri Batan <batanyuri@gmail.com>
2024-06-07 14:39:49 -0500

    Modified Mile's code to work in my specific case scenario

---
+++ BigVul/generate_cpgs.py
@@ -0,0 +1,39 @@
+#Thanks Miles
+import glob
+import os
+from multiprocessing import Pool, set_start_method
+from pathlib import Path
+
+source_directory = "Functions/"
+output_directory = "CPG/"
+joern_path = "/Users/yurischool/joern/joern-cli/"
+temp_joern_files_location = "Temp/"
+
+done_files = {done.stem for done in Path(output_directory).iterdir()}  # Set for faster lookups
+
+def joern_parse(source_file):
+    sample_name = Path(source_file).stem
+
+    if sample_name in done_files:
+        print(f"{sample_name} already processed")
+        return
+
+    bin_file = Path(temp_joern_files_location) / f"{sample_name}.bin"
+    out_file = Path(output_directory) / sample_name
+
+    # Using f-strings for clearer formatting and Path objects for better file handling
+    os.system(f"{joern_path}joern-parse {source_file} --language c --output {bin_file}")
+    os.system(f"{joern_path}joern-export {bin_file} --repr cpg --out {out_file} --format dot")
+    os.remove(bin_file)
+
+
+if __name__ == '__main__':
+    set_start_method("spawn")
+    files = glob.glob(source_directory + "*.cpp")
+
+    with Pool(18) as p:
+        p.map(joern_parse, files)
+
+
+    # java_location = "~/.jdks/openjdk-19.0.2"
+    #os.environ["JAVA_HOME"] = java_location
